672a0cf0186e731a0a5b50d748bd272ae51478a4
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-29 15:49:15 +0100

    Test that Note instantiates with only one argument

--- note-tests.js
+++ note-tests.js
@@ -7,12 +7,20 @@
   };
 
   function ChecksIfNoteTakesAnArgument() {
-    var note = new Note('Bob')
+    var note = new Note('1')
     if (note.arguments === 0) {
       throw new Error("Incorrect number of arguments. Expected 1, got 0")
     };
   };
 
+  function ChecksIfNoteOnlyTakesOneArgument() {
+    var note = new Note('1', '2')
+    if (note.arguments > 1) {
+      throw new Error(`Incorrect number of arguments. Expected 1, got ${note.arguments}`)
+    }
+  }
+
   testExistenceOfNote();
   ChecksIfNoteTakesAnArgument();
+  ChecksIfNoteOnlyTakesOneArgument();
 })(this);

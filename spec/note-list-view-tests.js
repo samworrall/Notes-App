07b54d388543b0b81f098d0a922fa57ce2800993
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-30 17:11:14 +0100

    Add notesToHTML function

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -1,7 +1,12 @@
 (function(exports) {
   describe('NoteListView')
     it('Is instantiated with a NoteList object')
-    var list = new NoteList();
-    var listview = new NoteListView(list)
-    assert.isAnInstanceOf(listview.argument, NoteList)
+      var list = new NoteList();
+      list.createNewNote('placeholder')
+      var listview = new NoteListView(list);
+      assert.isAnInstanceOf(listview.argument, NoteList)
+
+  describe('#notesToHTML')
+    it('Returns the notes as an html list string')
+      assert.areEqual(listview.notesToHTML(), '<ul><li><div>placeholder</div></li></ul>')
 })(this);
---
+++ src/note-list-view.js
@@ -0,0 +1,17 @@
+(function(exports) {
+  function NoteListView(notelist) {
+    this.argument = arguments[0]
+    this.notelist = notelist
+  }
+
+  NoteListView.prototype.notesToHTML = function() {
+    var allnotes = this.notelist.returnNotes()
+    var html = []
+    allnotes.forEach(function(note) {
+       html.push('<li><div>' + note.displayNote() + '</div></li>')
+     })
+     return '<ul>' + html.join('') + '</ul>'
+   }
+
+  exports.NoteListView = NoteListView;
+})(this);

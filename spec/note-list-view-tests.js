d09c7fe37923a765bf32ddb6092bece665ee57a8
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-30 17:25:04 +0100

    Working for multiple notes to html format

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -7,8 +7,13 @@
       assert.isAnInstanceOf(listview.argument, NoteList)
 
   describe('#notesToHTML')
-    it('Returns the notes as an html list string')
+    it('Returns the note as an html list string')
       assert.areEqual(listview.notesToHTML(), '<ul><li><div>placeholder</div></li></ul>')
+
+    it('Returns all notes as an html list string')
+      list.createNewNote('placeholder2')
+      var listview2 = new NoteListView(list)
+      assert.areEqual(listview2.notesToHTML(), '<ul><li><div>placeholder</div></li><li><div>placeholder2</div></li></ul>')
 
     it('Returns an empty string')
       var list2 = new NoteList();

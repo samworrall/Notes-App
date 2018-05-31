e359732c07066adb03545ac94920b383045be3eb
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-31 12:05:20 +0100

    Note-controller can add a note list to the html page

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,8 +1,26 @@
 appDiv = document.getElementById('app')
 contents = appDiv.innerHTML
 
-function changeWord(contents) {
-  contents.innerHTML = 'howdy'
+function changeContents(contents, replacement) {
+  contents.innerHTML = replacement
 }
 
-changeWord(appDiv);
+changeContents(appDiv, 'howdy');
+
+(function(exports) {
+  function NoteController(notelistmodel) {
+    notelistmodel.createNewNote('Favourite drink: seltzer')
+    var notelistview = new NoteListView(notelistmodel)
+
+    NoteController.prototype.addToHTML = function() {
+      var htmlString = notelistview.notesToHTML();
+      changeContents(appDiv, htmlString);
+    }
+  }
+  exports.NoteController = NoteController
+})(this);
+
+var note = new Note
+var listmodel = new NoteList
+controller = new NoteController(listmodel)
+controller.addToHTML();

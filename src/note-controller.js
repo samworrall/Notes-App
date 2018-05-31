e553076ef5c197bee9b1c92a9f3b69a484e0f5c7
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-31 13:29:57 +0100

    Create note-list-controller-tests file

--- src/note-controller.js
+++ src/note-controller.js
@@ -7,24 +7,24 @@ function changeContents(contents, replacement) {
 
 changeContents(appDiv, 'howdy');
 
-(function(exports) {
-  function NoteController(notelistmodel) {
-    this.notelist = notelistmodel
-    this.notelistview = new NoteListView(this.notelist)
-
-    NoteController.prototype.addNote = function(text) {
-      this.notelist.createNewNote(text);
-    }
-
-    NoteController.prototype.addToHTML = function() {
-      var htmlString = this.notelistview.notesToHTML();
-      changeContents(appDiv, htmlString);
-    }
-  }
-  exports.NoteController = NoteController
-})(this);
-
-var listmodel = new NoteList
-controller = new NoteController(listmodel)
-controller.addNote('Favourite drink: seltzer');
-controller.addToHTML();
+// (function(exports) {
+//   function NoteController(notelistmodel) {
+//     this.notelist = notelistmodel
+//     this.notelistview = new NoteListView(this.notelist)
+//
+//     NoteController.prototype.addNote = function(text) {
+//       this.notelist.createNewNote(text);
+//     }
+//
+//     NoteController.prototype.addToHTML = function() {
+//       var htmlString = this.notelistview.notesToHTML();
+//       changeContents(appDiv, htmlString);
+//     }
+//   }
+//   exports.NoteController = NoteController
+// })(this);
+//
+// var listmodel = new NoteList
+// controller = new NoteController(listmodel)
+// controller.addNote('Favourite drink: seltzer');
+// controller.addToHTML();

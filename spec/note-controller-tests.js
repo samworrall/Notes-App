90abb28362d62a64996b91100da75df2896bd829
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-01 10:30:19 +0100

    TDD note-controller with doubles

--- spec/note-controller-tests.js
+++ spec/note-controller-tests.js
@@ -1,11 +1,53 @@
 (function(exports) {
-  var notecontroller = new NoteController()
-  // var body = document.getElementByTagName("body")
-  // var mockElement = document.createElement("div")
-  // mockElement.id = "app"
 
+  function Element() {
+    this.text = 'hello'
+  }
+
+  Element.prototype = {
+    innerHTML: function() {
+      return this.text
+    }
+  }
+
+  function DocumentDouble() {
+  }
+
+  DocumentDouble.prototype = {
+    getElementById: function() {
+      return new Element()
+    }
+  }
+
+  function NoteListModelDouble() {
+    this.createNewNoteCount = 0;
+  }
+
+  NoteListModelDouble.prototype = {
+    createNewNote: function() {
+      this.createNewNoteCount++;
+    }
+  }
+
+  var doc = new DocumentDouble();
+  var notelistmodel = new NoteListModelDouble();
+  var notecontroller = new NoteController(notelistmodel)
+  NoteListView.prototype.notesToHTML = function() {
+    return '<ul><li><div>test</div></li></ul>'
+  }
 
   describe('NoteListModel')
     it('Checks if NoteListModel exists')
     assert.exists(notecontroller)
+
+  describe('#addNote')
+    it('Adds a new note behind the scenes')
+    notecontroller.addNote('test')
+    assert.areEqual(notelistmodel.createNewNoteCount, 1)
+
+  describe('#addToHTML')
+    it('Returns an html list string')
+    console.log(notecontroller.addToHTML('app', doc))
+    assert.areEqual(notecontroller.addToHTML('app', doc), '<ul><li><div>test</div></li></ul>')
+
 })(this);
--- src/note-controller.js
+++ src/note-controller.js
@@ -1,32 +1,27 @@
-function GetElement(id, doc = document) {
-  appDiv = document.getElementById(id)
+function getElement(id, doc) {
+  appDiv = doc.getElementById(id)
   return appDiv
 }
 
-function changeContents(id, replacement, doc = document) {
-  GetElement(id).innerHTML = replacement
+function changeContents(id, replacement, doc) {
+  var newelement = getElement(id, doc).innerHTML = replacement
+  return newelement
 }
-
-// changeContents('app', 'howdy');
 
 (function(exports) {
   function NoteController(notelistmodel) {
-//     this.notelist = notelistmodel
-//     this.notelistview = new NoteListView(this.notelist)
-//
-//     NoteController.prototype.addNote = function(text) {
-//       this.notelist.createNewNote(text);
-//     }
-//
-//     NoteController.prototype.addToHTML = function() {
-//       var htmlString = this.notelistview.notesToHTML();
-//       changeContents(appDiv, htmlString);
-//     }
+    this.notelist = notelistmodel
+    this.notelistview = new NoteListView(this.notelist)
+
+    NoteController.prototype.addNote = function(text) {
+      this.notelist.createNewNote(text);
+    }
+
+    NoteController.prototype.addToHTML = function(id, doc = document) {
+      var htmlString = this.notelistview.notesToHTML();
+      return changeContents(id, htmlString, doc);
+    }
   }
+
   exports.NoteController = NoteController
 })(this);
-//
-// var listmodel = new NoteList
-// controller = new NoteController(listmodel)
-// controller.addNote('Favourite drink: seltzer');
-// controller.addToHTML();
